2a91061b23e720d1c33d1852111998b58bb146dc
jesuslopez-src <jesuslopezcm@gmail.com>
2023-01-28 17:42:03 -0400

    Se modificaron los componentes TaskForm_Refs y el TaskForm_State para seguir los últimos cambios en TaskForm_Reducer

--- src/components/tasks/TaskForm_Refs.tsx
+++ src/components/tasks/TaskForm_Refs.tsx
@@ -4,13 +4,15 @@ import Button from "../UI/Button";
 import btn_styles from "../UI/Button.module.css";
 import type { task } from "../../types/tasks";
 
-interface Props{
-    createTask : (task:task) => void
+interface Props {
+    usingLocalStorage: boolean,
+    createTask: (task: task) => void,
+    changedRadio: (input: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 const priorityDefault = "Urgente";
 
-const TaskForm = (props:Props) => {
+const TaskForm = (props: Props) => {
 
     const taskTitleRef = useRef<HTMLInputElement>(null)
     const descriptionRef = useRef<HTMLTextAreaElement>(null)
@@ -18,18 +20,18 @@ const TaskForm = (props:Props) => {
     const priorityRef = useRef<HTMLSelectElement>(null)
     const dateRef = useRef<HTMLInputElement>(null)
 
-    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        if(taskTitleRef.current!.value.trim() !== ""){
-            const newTask:task = {
-                id : Math.random().toString(),
-                title : taskTitleRef.current!.value,
-                description : descriptionRef.current!.value,
-                owner : taskOwnerRef.current!.value,
-                priority : priorityRef.current!.value,
-                deadline : dateRef.current!.value
+        if (taskTitleRef.current!.value.trim() !== "") {
+            const newTask: task = {
+                id: Math.random().toString(), // find better way to create unique ids
+                title: taskTitleRef.current!.value,
+                description: descriptionRef.current!.value,
+                owner: taskOwnerRef.current!.value,
+                priority: priorityRef.current!.value,
+                deadline: dateRef.current!.value
             }
-            
+
             props.createTask(newTask);
             taskTitleRef.current!.value = ""
             descriptionRef.current!.value = "";
@@ -39,33 +41,43 @@ const TaskForm = (props:Props) => {
         }
     }
 
-    return( 
-    <form onSubmit={handleSubmit} className={styles.formulario}>
-        <div className = {styles["form-field"]} >
-            <label htmlFor="task-title">Task:</label>
-            <input ref={taskTitleRef} id="task-title" autoFocus type="text" placeholder="name of task" />
-        </div>
-        <div className = {styles["form-field"]} >
-            <label htmlFor="task-owner">Task Owner:</label>
-            <input ref= {taskOwnerRef} id="task-owner" type="text" placeholder="owner of the task" />
-        </div>
-        <div className = {styles["form-field"]} >
-            <label htmlFor="task-description">Task Description:</label>
-            <textarea ref={descriptionRef} id="task-description" cols={18} rows={4} placeholder="Descripción de la tarea"></textarea>
-        </div>
-        <div className = {styles["form-field"]} >
-            <label htmlFor="task-priority">Task Priority:</label>
-            <select ref={priorityRef} id="task-priority" defaultValue={priorityDefault}>
-                <option value={priorityDefault}>{priorityDefault}</option>
-                <option value="Aplazable">Aplazable</option>
-            </select>
-        </div>
-        <div className = {styles["form-field"]} >
-            <label htmlFor="task-deadline">Task Deadline:</label>
-            <input ref={dateRef} type="date" id="task-deadline" />
-        </div>
-        <Button classes={btn_styles["add-task"]} type="submit">Guardar</Button>
-    </form>
+    return (
+        <form onSubmit={handleSubmit} className={styles.formulario}>
+            <div className={styles["form-field-column"]} >
+                <label htmlFor="task-title">Task:</label>
+                <input ref={taskTitleRef} id="task-title" autoFocus type="text" placeholder="name of task" />
+            </div>
+            <div className={styles["form-field-column"]} >
+                <label htmlFor="task-owner">Task Owner:</label>
+                <input ref={taskOwnerRef} id="task-owner" type="text" placeholder="owner of the task" />
+            </div>
+            <div className={styles["form-field-column"]} >
+                <label htmlFor="task-description">Task Description:</label>
+                <textarea ref={descriptionRef} id="task-description" cols={18} rows={4} placeholder="Descripción de la tarea"></textarea>
+            </div>
+            <div className={styles["form-field-column"]} >
+                <label htmlFor="task-priority">Task Priority:</label>
+                <select ref={priorityRef} id="task-priority" defaultValue={priorityDefault}>
+                    <option value={priorityDefault}>{priorityDefault}</option>
+                    <option value="Aplazable">Aplazable</option>
+                </select>
+            </div>
+            <div className={styles["form-field-column"]} >
+                <label htmlFor="task-deadline">Task Deadline:</label>
+                <input ref={dateRef} type="date" id="task-deadline" />
+            </div>
+            <div className={[styles["form-field-column"], styles["smaller-font"]].join(' ')} >
+                <label>
+                    <input onChange={props.changedRadio} checked={props.usingLocalStorage} type="radio" name="choice" value="local_storage" />
+                    Tasks in LocalStorage
+                </label>
+                <label>
+                    <input onChange={props.changedRadio} checked={!props.usingLocalStorage} type="radio" name="choice" value="remote_DB" />
+                    Tasks in Remote Database
+                </label>
+            </div>
+            <Button classes={btn_styles["add-task"]} type="submit">Guardar</Button>
+        </form>
     )
 }
 
--- src/components/tasks/TaskForm_State.tsx
+++ src/components/tasks/TaskForm_State.tsx
@@ -4,32 +4,34 @@ import Button from "../UI/Button";
 import btn_styles from "../UI/Button.module.css";
 import type { task } from "../../types/tasks";
 
-interface Props{
-    createTask : (task:task) => void
+interface Props {
+    usingLocalStorage: boolean,
+    createTask: (task: task) => void,
+    changedRadio: (input: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 const priorityDefault = "Urgente";
 
-const TaskForm = (props:Props) => {
+const TaskForm = (props: Props) => {
 
-    const [taskTitle,setTaskTitle] = useState("");
-    const [taskOwner,setTaskOwner] = useState("");
-    const [taskDescription,setTaskDescription] = useState("");
-    const [taskPriority,setTaskPriority] = useState(priorityDefault);
-    const [taskDate,setTaskDate] = useState("");
+    const [taskTitle, setTaskTitle] = useState("");
+    const [taskOwner, setTaskOwner] = useState("");
+    const [taskDescription, setTaskDescription] = useState("");
+    const [taskPriority, setTaskPriority] = useState(priorityDefault);
+    const [taskDate, setTaskDate] = useState("");
 
-    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        if(taskTitle.trim() !== ""){
-            const newTask:task = {
-                id : Math.random().toString(),
-                title : taskTitle,
-                owner : taskOwner,
-                description : taskDescription,
-                priority : taskPriority,
-                deadline : taskDate
+        if (taskTitle.trim() !== "") {
+            const newTask: task = {
+                id: Math.random().toString(),
+                title: taskTitle,
+                owner: taskOwner,
+                description: taskDescription,
+                priority: taskPriority,
+                deadline: taskDate
             }
-            
+
             props.createTask(newTask);
             setTaskTitle("");
             setTaskDescription("")
@@ -39,54 +41,64 @@ const TaskForm = (props:Props) => {
         }
     }
 
-    const taskTitleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const taskTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTaskTitle(e.target.value);
     }
 
-    const taskOwnerChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const taskOwnerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTaskOwner(e.target.value);
     }
 
-    const taskDescriptionChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+    const taskDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setTaskDescription(e.target.value);
     }
 
-    const taskPriorityChange = (e:React.ChangeEvent<HTMLSelectElement>) => {
+    const taskPriorityChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setTaskPriority(e.target.value);
     }
 
-    const taskDateChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const taskDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTaskDate(e.target.value);
     }
 
-    return( 
-    <form onSubmit={handleSubmit} className={styles.formulario}>
-        <div className = {styles["form-field"]} >
-            <label htmlFor="task-title">Task:</label>
-            <input onChange={taskTitleChange} id="task-title" value={taskTitle} autoFocus type="text" placeholder="name of task" />
-        </div>
-        <div className = {styles["form-field"]} >
-            <label htmlFor="task-owner">Task Owner:</label>
-            <input onChange={taskOwnerChange} id="task-owner" value={taskOwner} type="text" placeholder="owner of the task" />
-        </div>
-        <div className = {styles["form-field"]} >
-            <label htmlFor="task-description">Task Description:</label>
-            <textarea onChange={taskDescriptionChange} id="task-description" value={taskDescription} cols={18} rows={4} placeholder="Descripción de la tarea">
-            </textarea>
-        </div>
-        <div className = {styles["form-field"]} >
-            <label htmlFor="task-priority">Task Priority:</label>
-            <select onChange={taskPriorityChange} id="task-priority" value={taskPriority}>
-                <option value={priorityDefault}>{priorityDefault}</option>
-                <option value="Aplazable">Aplazable</option>
-            </select>
-        </div>
-        <div className = {styles["form-field"]} >
-            <label htmlFor="task-deadline">Task Deadline:</label>
-            <input onChange={taskDateChange} value={taskDate} type="date" id="task-deadline" />
-        </div>
-        <Button classes={btn_styles["add-task"]} type="submit">Guardar</Button>
-    </form>
+    return (
+        <form onSubmit={handleSubmit} className={styles.formulario}>
+            <div className={styles["form-field-column"]} >
+                <label htmlFor="task-title">Task:</label>
+                <input onChange={taskTitleChange} id="task-title" value={taskTitle} autoFocus type="text" placeholder="name of task" />
+            </div>
+            <div className={styles["form-field-column"]} >
+                <label htmlFor="task-owner">Task Owner:</label>
+                <input onChange={taskOwnerChange} id="task-owner" value={taskOwner} type="text" placeholder="owner of the task" />
+            </div>
+            <div className={styles["form-field-column"]} >
+                <label htmlFor="task-description">Task Description:</label>
+                <textarea onChange={taskDescriptionChange} id="task-description" value={taskDescription} cols={18} rows={4} placeholder="Descripción de la tarea">
+                </textarea>
+            </div>
+            <div className={styles["form-field-column"]} >
+                <label htmlFor="task-priority">Task Priority:</label>
+                <select onChange={taskPriorityChange} id="task-priority" value={taskPriority}>
+                    <option value={priorityDefault}>{priorityDefault}</option>
+                    <option value="Aplazable">Aplazable</option>
+                </select>
+            </div>
+            <div className={styles["form-field-column"]} >
+                <label htmlFor="task-deadline">Task Deadline:</label>
+                <input onChange={taskDateChange} value={taskDate} type="date" id="task-deadline" />
+            </div>
+            <div className={[styles["form-field-column"], styles["smaller-font"]].join(' ')} >
+                <label>
+                    <input onChange={props.changedRadio} checked={props.usingLocalStorage} type="radio" name="choice" value="local_storage" />
+                    Tasks in LocalStorage
+                </label>
+                <label>
+                    <input onChange={props.changedRadio} checked={!props.usingLocalStorage} type="radio" name="choice" value="remote_DB" />
+                    Tasks in Remote Database
+                </label>
+            </div>
+            <Button classes={btn_styles["add-task"]} type="submit">Guardar</Button>
+        </form>
     )
 }
 
